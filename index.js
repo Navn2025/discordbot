572b556d88f8708691c655d69ae2510b571899f7
Navn2025 <navneett546@gmail.com>
2025-09-26 01:23:51 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express=require('express');
 const app=express();
-const PORT=process.env.PORT||3000;
+const PORT=3000;
 
 const {GoogleGenAI}=require('@google/genai');
 const {Client, GatewayIntentBits}=require('discord.js');
